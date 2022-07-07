6ed6c14d24a7dc2b4c6c99141ede4c4462fa81e0
Christopher Eeles <christopher.eeles@outlook.com>
2022-07-06 22:26:51 -0400

    Update nci_alamanac_analysis script to use endoaggregate and mergeAssays methods

--- CoreGx/nci_almanac_analysis.R
+++ CoreGx/nci_almanac_analysis.R
@@ -1,8 +1,10 @@
 library(PharmacoGx)
 library(data.table)
-library(BiocParallel)
 
-nci <- readRDS(file.path(".local_data", "NCI_ALMANAC_2017.rds"))
+# set this to your data directory!
+data_dir <- ".local_data"
+
+nci <- readRDS(file.path(data_dir, "NCI_ALMANAC_2017.rds"))
 
 
 # -- Fit the Hill curve model to monotherapy drugs and compute associated metrics
@@ -28,6 +30,7 @@ treatmentResponse(nci) |>
                 E_inf=fit[['E_inf']] / 100,
                 EC50=fit[['EC50']],
                 Rsquare=attributes(fit)$Rsquare,
+                viability=mean(viability, na.rm=TRUE) / 100,
                 auc=auc,
                 ic50=ic50
             )
@@ -39,15 +42,14 @@ treatmentResponse(nci) |>
     tre
 }) -> m1
 
-bench::system_time({
 # -- Attach the Hill parameters from the monotherapy profiles to the combination data
-# Extract the monotherapy fits
+bench::system_time({
 tre |>
     endoaggregate(
         assay="sensitivity",
         target="combo_raw",
         subset=treatment2id != "",
-        viability=mean(viability) / 100,
+        viability_combo=mean(viability) / 100,
         by=c("treatment1id", "treatment1dose", "treatment2id", "treatment2dose",
             "sampleid")
     ) ->
@@ -64,35 +66,16 @@ tre |>
     mergeAssays(
         "combo_raw",
         "mono_profiles",
-        by=c(treatment2id="treatment1id", "sampleid"),
+        by.x=c("treatment2id", "sampleid"),
+        by.y=c("treatment1id", "sampleid"),
         suffixes=c("_1", "_2")
     ) ->
     tre
 }) -> m3
 
-# -- predict viability for each drug in our combination
-
-bench::system_time({
-tre |>
-    endoaggregate(
-        assay="combo_raw",
-        target="combo_raw",
-        {
-            v1 <- PharmacoGx:::.Hill(log10(treatment1dose),
-                c(HS_1, E_inf_1, log10(EC50_1)))
-            v2 <-  PharmacoGx:::.Hill(log10(treatment2dose),
-                c(HS_2, E_inf_2, log10(EC50_2)))
-            list(viability_1=v1, viability_2=v2)
-        },
-        by=assayKeys(tre, "combo_raw"),
-        enlist=FALSE
-    ) ->
-    ntre1
-}) -> m4
-
 # -- compute our drug synergy metrics
 bench::system_time({
-ntre1 |>
+tre |>
     endoaggregate(
         assay="combo_raw",
         HSA=min(viability_1, viability_2),
@@ -114,34 +97,35 @@ ntre1 |>
             EC50_1=EC50_1,
             EC50_2=EC50_2
         ),
+        by=assayKeys(tre, "combo_raw")
+    ) ->
+    ntre1
+}) -> m4
+
+
+# -- compute combination index and score
+bench::system_time({
+ntre1 |>
+    endoaggregate(
+        assay="combo_raw",
+        HSA_CI=viability_combo / HSA,
+        HSA_score=HSA - viability_combo,
+        Bliss_CI=viability_combo / Bliss,
+        Bliss_score=Bliss - viability_combo,
+        ZIP_CI=viability_combo / ZIP,
+        ZIP_score=ZIP - viability_combo,
+        Loewe_CI=viability_combo / Loewe,
+        Loewe_score=Loewe - viability_combo,
         by=assayKeys(ntre1, "combo_raw")
     ) ->
     ntre2
 }) -> m5
 
-
-# -- compute combination index and score
-bench::system_time({
-ntre2 |>
-    endoaggregate(
-        assay="combo_raw",
-        HSA_CI=viability / HSA,
-        HSA_score=HSA - viability,
-        Bliss_CI=viability / Bliss,
-        Bliss_score=Bliss - viability,
-        ZIP_CI=viability / ZIP,
-        ZIP_score=ZIP - viability,
-        Loewe_score=Loewe - viability,
-        Loewe_CI=viability / Loewe,
-        by=assayKeys(ntre2, "combo_raw")
-    ) ->
-    ntre2
-}) -> m6
-
 # -- summarize our combination scores
 bench::system_time({
 ntre2 |>
     endoaggregate(
+        subset=Rsquare_1 > 0.5 & Rsquare_2 > 0.5, # remove bad fits
         assay="combo_raw",
         target="combo_profiles",
         HSA_CI=mean(HSA_CI, na.rm=TRUE),
@@ -155,7 +139,7 @@ ntre2 |>
         by=c("treatment1id", "treatment2id", "sampleid")
     ) ->
     ntre3
-}) -> m7
+}) -> m6
 
 # -- extract our combo scores and have a look
 combo_prof <- ntre3$combo_profiles
@@ -163,11 +147,34 @@ cor(combo_prof[,
     .(HSA_score, Bliss_score, Loewe_score, ZIP_score)
 ], method="pearson", use="complete.obs")
 
+# rank by most conservative estimate of synergy
+combo_prof[,
+    min_score := pmin(HSA_score, Bliss_score, Loewe_score, ZIP_score,
+        na.rm=TRUE)
+]
+combo_prof[,
+    max_CI := pmax(HSA_CI, Bliss_CI, Loewe_CI, ZIP_CI)
+]
+combo_prof[
+    order(-min_score),
+    .SD, .SDcols=patterns("*_score"),
+    by=c(assayKeys(ntre3, "combo_profiles"))
+] |> head(50)
+
 ## _score columns calculate the difference between null model viability and
 ## observed viability, where positive values are good
-combo_prof[
-    order(-Bliss_score),
-    lapply(.SD, mean, na.rm=TRUE),
+combo_prof[,
+    lapply(.SD, median, na.rm=TRUE),
     .SDcols=is.numeric,
     by=c("treatment1id", "treatment2id")
-]
+][order(-min_score)] |> head(10)
+
+combo_prof[,
+    lapply(.SD, median, na.rm=TRUE),
+    .SDcols=is.numeric,
+    by=c("sampleid")
+][order(-min_score)] |> head(25)
+
+## -- save the results to compare against SYNERGxDB
+setorderv(combo_prof, c("min_score"), order=-1L)
+fwrite(combo_prof, file=file.path(data_dir, "nci_almanac_synergy_profiles.csv"))
